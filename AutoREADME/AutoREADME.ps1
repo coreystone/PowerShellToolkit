2aa5de05b2359f00683dc2abd1fae130f1d14e0b
Corey Stone <coreyjs@uci.edu>
2019-10-08 11:01:42 -0700

    Updated GUI; added error checking and fixed bugs

--- AutoREADME/AutoREADME.ps1
+++ AutoREADME/AutoREADME.ps1
@@ -1,120 +1,125 @@
-Remove-PSDrive -name "X" -Force;
+
 # Authenticate and map PSDrive to devnas13\images
 $credential = Get-Credential
 $credential.Password | ConvertFrom-SecureString | Set-Content c:\temp\password.txt
 $encrypted = Get-Content c:\temp\password.txt | ConvertTo-SecureString
 $credential = New-Object System.Management.Automation.PsCredential($credential.Username, $encrypted)
-
-New-PSDrive -name "X"� -PSProvider FileSystem -Root \\domain\images -Persist -Credential $credential
+New-PSDrive -name “X” -PSProvider FileSystem -Root \\domain\images -Persist -Credential $credential
  
-
 function browse () {
-    Add-Type -AssemblyName System.Windows.Forms
-    $browser = New-Object System.Windows.Forms.FolderBrowserDialog
-    $browser.SelectedPath = "\\domain\images"
-    $null = $browser.ShowDialog()
-    Write-Host $browser.SelectedPath
-    $userPath = $browser.SelectedPath
-    $textBox1.Text = $userPath
-    return $userPath
-}
-
-function enterButton ($title,$folderpath, $note, $initials) {
-###################Load Assembly for creating form & button######
-[void][System.Reflection.Assembly]::LoadWithPartialName( “System.Windows.Forms”)
-[void][System.Reflection.Assembly]::LoadWithPartialName( “Microsoft.VisualBasic”)
-
-#####Define the form size & placement
-$form = New-Object “System.Windows.Forms.Form”;
-$form.Width = 550;
-$form.Height = 150;
-$form.Text = $title;
-$form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen;
-
-##############Define text label1
-$textLabel1 = New-Object “System.Windows.Forms.Label”;
-$textLabel1.Left = 10;
-$textLabel1.Top = 10;
-$textLabel1.Text = $folderpath;
-
-##############Define text label2
-$textLabel2 = New-Object “System.Windows.Forms.Label”;
-$textLabel2.Left = 10;
-$textLabel2.Top = 40;
-$textLabel2.Text = $note;
-
-##############Define text label3
-$textLabel3 = New-Object “System.Windows.Forms.Label”;
-$textLabel3.Left = 10;
-$textLabel3.Top = 70;
-$textLabel3.Text = $initials;
-
-############Define text box1 for input
-$textBox1 = New-Object “System.Windows.Forms.TextBox”;
-$textBox1.Left = 110;
-$textBox1.Top = 10;
-$textBox1.width = 400;
-
-############Define text box2 for input
-$textBox2 = New-Object “System.Windows.Forms.TextBox”;
-$textBox2.Left = 110;
-$textBox2.Top = 40;
-$textBox2.width = 400;
-
-############Define text box3 for input
-$textBox3 = New-Object “System.Windows.Forms.TextBox”;
-$textBox3.Left = 110;
-$textBox3.Top = 70;
-$textBox3.width = 50;
-
-#############Define default values for the input boxes
-$textBox1.Text = “\\domain\images\”;
-$textBox2.Text = “”;
-$textBox3.Text = “”;
-
-#############define browseButton
-$browseButton = New-Object “System.Windows.Forms.Button”;
-$browseButton.Left = 420;
-$browseButton.Top = 40;
-$browseButton.Width = 100;
-$browseButton.Text = “Browse”;
-
-#############define enterButton
-$enterButton = New-Object “System.Windows.Forms.Button”;
-$enterButton.Left = 420;
-$enterButton.Top = 70;
-$enterButton.Width = 100;
-$enterButton.Text = “Enter”;
-
-############# This is when you have to close the form after getting values
-$eventHandler = [System.EventHandler]{
-$textBox1.Text;
-$textBox2.Text;
-$textBox3.Text;
-$form.Close();
-$browseButton.Add_Click({browse});
-$enterButton.Add_Click($eventHandler) ;
-
-#############Add controls to all the above objects defined
-$form.Controls.Add($browseButton);
-$form.Controls.Add($enterButton);
-$form.Controls.Add($textLabel1);
-$form.Controls.Add($textLabel2);
-$form.Controls.Add($textLabel3);
-$form.Controls.Add($textBox1);
-$form.Controls.Add($textBox2);
-$form.Controls.Add($textBox3);
-$ret = $form.ShowDialog();
-
-#################return values
-return $textBox1.Text, $textBox2.Text, $textBox3.Text
-}
-
-$return= enterButton “Auto README” “Folder path” “Add note” “Initials”
+ Add-Type -AssemblyName System.Windows.Forms
+ $browser = New-Object
+System.Windows.Forms.FolderBrowserDialog
+ $browser.SelectedPath =
+"\\domain\images"
+ $null = $browser.ShowDialog()
+ Write-Host $browser.SelectedPath
+ $userPath = $browser.SelectedPath
+ $textBox1.Text = $userPath
+ return $userPath
+ }
+ 
+ 
+ function button ($title,$folderpath, $note,
+$initials) {
+ #########Load Assembly for creating form & button######
+ [void][System.Reflection.Assembly]::LoadWithPartialName(
+"System.Windows.Forms")
+ [void][System.Reflection.Assembly]::LoadWithPartialName(
+"Microsoft.VisualBasic")
+ #####Define the form size & placement
+ $form = New-Object "System.Windows.Forms.Form";
+ $form.Width = 550;
+ $form.Height = 150;
+ $form.Text = $title;
+ $form.StartPosition =
+[System.Windows.Forms.FormStartPosition]::CenterScreen;
+ 
+ ##############Define text label1
+ $textLabel1 = New-Object "System.Windows.Forms.Label";
+ $textLabel1.Left = 10;
+ $textLabel1.Top = 10;
+ $textLabel1.Text = $folderpath
+ 
+ ##############Define text label2
+ $textLabel2 = New-Object "System.Windows.Forms.Label";
+ $textLabel2.Left = 10;
+ $textLabel2.Top = 40;
+ $textLabel2.Text = $note;
+ ##############Define text label3
+ $textLabel3 = New-Object "System.Windows.Forms.Label";
+ $textLabel3.Left = 10;
+ $textLabel3.Top = 70;
+ $textLabel3.Text = $initials;
+ 
+ ############Define text box1 for input
+ $textBox1 = New-Object "System.Windows.Forms.TextBox";
+ $textBox1.Left = 110;
+ $textBox1.Top = 10;
+ $textBox1.width = 300;
+ 
+ ############Define text box2 for input
+ $textBox2 = New-Object "System.Windows.Forms.TextBox";
+ $textBox2.Left = 110;
+ $textBox2.Top = 40;
+ $textBox2.width = 410;
+ 
+ ############Define text box3 for input
+ $textBox3 = New-Object "System.Windows.Forms.TextBox";
+ $textBox3.Left = 110;
+ $textBox3.Top = 70;
+ $textBox3.width = 50;
+ 
+ #############Define default values for the input boxes
+ $defaultValue = ""
+ $textBox1.Text = "\\domain\IMAGES";;
+ $textBox2.Text = "";
+ $textBox3.Text = "";
+ 
+ 
+ 
+ #############define Browse button
+ $browseButton = New-Object "System.Windows.Forms.Button";
+ $browseButton.Left = 420;
+ $browseButton.Top = 10;
+ $browseButton.Width = 100;
+ $browseButton.Text = "Browse";
+ 
+ #############define enterButton
+ $enterButton = New-Object "System.Windows.Forms.Button";
+ $enterButton.Left = 420;
+ $enterButton.Top = 70;
+ $enterButton.Width = 100;
+ $enterButton.Text = "OK";
+ 
+ ############# This is when you have to close the form after getting values
+ $eventHandler = [System.EventHandler]{
+ $textBox1.Text;
+ $textBox2.Text;
+ $textBox3.Text;
+ $form.Close();};
+ $browseButton.Add_Click({browse});
+ $enterButton.Add_Click($eventHandler) ;
+ 
+ #############Add controls to all the above objects defined
+ $form.Controls.Add($browseButton);
+ $form.Controls.Add($enterButton);
+ $form.Controls.Add($textLabel1);
+ $form.Controls.Add($textLabel2);
+ $form.Controls.Add($textLabel3);
+ $form.Controls.Add($textBox1);
+ $form.Controls.Add($textBox2);
+ $form.Controls.Add($textBox3);
+ $ret = $form.ShowDialog();
+ 
+ #################return values
+ return $textBox1.Text, $textBox2.Text, $textBox3.Text
+ }
+ $return= button "Auto README" "Folder Path" "Add Note" "Initials"
 $folderpath = $return[0] 
 $userNote = $return[1]
 $initials = $return[2]
-If ($test -notmatch '.+?\\$') {$folderpath += “\”}
+ 
 $folderpath += “README.txt”
 $path = [System.IO.DirectoryInfo]$folderpath
 
@@ -125,6 +130,8 @@ $notes =  Get-Content $path | Select-Object -Index (34..$numLines)
 
 $lines = get-content $path
 $numLines = $lines.Count
+
+#$notes =  Get-Content $path | Select-Object -Index (30..$numLines)
 
 
 $model = (Get-WmiObject -Class:Win32_ComputerSystem).Model
@@ -223,6 +230,4 @@ Add-Content $path $newNote
 
 
 # Unmap the PSDrive
-Remove-PSDrive -name "X" -Force;
-
-
+Remove-PSDrive -name "X" -Force
--- AutoZScaler/AutoZScaler.ps1
+++ AutoZScaler/AutoZScaler.ps1
@@ -2,17 +2,21 @@
 $credential = Get-Credential
 $credential = New-Object System.Management.Automation.PsCredential($credential.Username, $credential.Password)
 New-PSDrive -name “X” -PSProvider FileSystem -Root \\domain -Persist -Credential $credential
+Write-Host Authenticated and connected to \\domain
 
 
-#### Add the configuration script ####
+# Add the configuration script
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v AutoConfigURL /t REG_SZ /d "http://PACFILE.pac" /f
+Write-Host Modified auto-configuration script
 
 
-
-#### Import the certificate ####
+# Import the certificate
 X:
 $zScalerCert = (Get-ChildItem -Path 'CERT.crt')
 $zScalerCert | Import-Certificate -CertStoreLocation Cert:\CurrentUser\Root
+Write-Host Imported zScaler root certificate
+
 
 # Unmap the PSDrive
 Remove-PSDrive -name "X" -Force
+Write-Host zScaler successfully configured
