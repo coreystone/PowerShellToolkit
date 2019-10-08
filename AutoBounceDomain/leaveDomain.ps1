65748781bd21003217d3b703aae9502da93689e1
Corey Stone <coreyjs@uci.edu>
2019-10-08 11:59:20 -0700

    Created script to automatically bounce (leave and join) domain & restart for troubleshooting purposes

---
+++ AutoBounceDomain/leaveDomain.ps1
@@ -0,0 +1,8 @@
+﻿# Leave the domain and join TEMP workgroup
+Add-Computer -WorkgroupName TEMP
+
+# Add one-time task to run script to re-join the domain
+Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
+Set-ItemProperty -Path . -Name rejoinDomain -Value "\\domain\rejoinDomain.ps1"
+
+Restart-Computer
---
+++ AutoBounceDomain/run_autobouncedomain.bat
@@ -0,0 +1,3 @@
+@echo off
+Powershell.exe -ExecutionPolicy Bypass -File  "\\domain\leavedomain.ps1"
+pause
